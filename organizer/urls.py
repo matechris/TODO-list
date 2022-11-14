1b759ae027400617016f8cd52290cf15d5ac70ef
Khrystyna Skrypko <christina.skrypko@gmail.com>
2022-11-14 11:06:40 +0200

    Add tag list page

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 
-from organizer.views import TaskListView
+from organizer.views import TaskListView, TagListView
 
 urlpatterns = [
     path(
         "",
         TaskListView.as_view(),
         name="task-list"
-    )
+    ),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag-list"
+    ),
 ]
 
 app_name = "organizer"
--- organizer/views.py
+++ organizer/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
 
-from organizer.models import Task
+from organizer.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
+
+
+class TagListView(generic.ListView):
+    model = Tag
