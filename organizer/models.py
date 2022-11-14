ae1b132af13d6f13e0fb052d4c639e8f563b6d08
Khrystyna Skrypko <christina.skrypko@gmail.com>
2022-11-14 14:15:03 +0200

    Add str methods for models

--- organizer/models.py
+++ organizer/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Task(models.Model):
     content = models.TextField()
@@ -15,4 +18,7 @@ class Task(models.Model):
     )
 
     class Meta:
-        ordering = ["is_done", "created_at"]
+        ordering = ["is_done", "-created_at"]
+
+    def __str__(self) -> str:
+        return self.content
